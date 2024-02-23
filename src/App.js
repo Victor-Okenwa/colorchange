68f655951bf13497f798a85beda901c281efc793
Okenwa victor <okenwavictor003@gmail.com>
2024-02-23 18:09:26 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 
 function App() {
+  const [color, setColor] = useState();
+  // const handleColorChange = (color) => {
+  //   return {backgroundColor: color};
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="colorBox" style={{
+        backgroundColor: color
+      }}>
+        <h3>{color ? color : 'Enter color name'}</h3>
+      </div>
+
+      <input type="text"
+        autoFocus
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+        placeholder='Enter color'
+      />
+    </div >
   );
 }
 
